b73c351f38546ceca56d964a128884f0b7834cd5
Alexey R <alexey_r.98@ngs.ru>
2021-10-26 18:35:03 +0700

    Add: git hooks

--- installEnv.sh
+++ installEnv.sh
@@ -3,5 +3,6 @@ rm -v ~/.bashrc ~/.local/share/mc/bashrc ~/.tmux.conf
 cp -vf bash/.bashrc ~/.bashrc
 mkdir -vp ~/.local/share/mc/
 mkdir -vp ~/.githooks
+cp -v git/hooks/* ~/.githooks/
 cp -vf bash/bashrc_mc ~/.local/share/mc/bashrc
 cp -vf tmux/.tmux.conf ~/.tmux.conf
