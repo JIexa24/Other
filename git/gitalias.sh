1f15a745676795e502252f0af36b7fe9b2a34a87
Alexey R <alexey_r.98@ngs.ru>
2021-10-26 18:18:25 +0700

    -

---
+++ git/gitalias.sh
@@ -0,0 +1,17 @@
+git config --global alias.co checkout
+git config --global alias.cm "commit -m"
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%C(cyan)%h%Creset %ad | %s%C(cyan)%d%Creset [%C(bold blue)%an|%ae%Creset] %C(green)(%cr)%Creset' --graph --date=local --all"
+git config --global alias.ps push
+git config --global alias.pl pull
+git config --global alias.ft fetch
+git config --global alias.mg merge
+git config --global alias.cf config
+git config --global alias.ad add
+git config --global alias.cl clone
+
+#git config --global credential.helper cache --timeout 60000
+git config --global credential.helper store
+
+git config --global core.hooksPath ~/.githooks
---
+++ installEnv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -v ~/.bashrc ~/.local/share/mc/bashrc ~/.tmux.conf
+cp -vf bash/.bashrc ~/.bashrc
+mkdir -vp ~/.local/share/mc/
+mkdir -vp ~/.githooks
+cp -vf bash/bashrc_mc ~/.local/share/mc/bashrc
+cp -vf tmux/.tmux.conf ~/.tmux.conf
