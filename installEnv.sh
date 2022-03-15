0293d8838a5fd8b565f4a28162978f1672ea6325
Alexey R <alexey_r.98@ngs.ru>
2022-03-16 00:05:54 +0700

    Fix: install env

--- installEnv.sh
+++ installEnv.sh
@@ -2,7 +2,7 @@
 rm -vf ~/.bashrc ~/.local/share/mc/bashrc ~/.tmux.conf
 cp -vf bash/.bashrc ~/.bashrc
 mkdir -vp ~/.githooks
-cp -v git/hooks/* ~/.githooks/
+cp -vf git/hooks/* ~/.githooks/
 cp -vf tmux/.tmux.conf ~/.tmux.conf
 mkdir -vp ~/.bashrc.d
 [ -d "./bash/.bashrc.d" ] && cp -v bash/.bashrc.d/*.sh ~/.bashrc.d
