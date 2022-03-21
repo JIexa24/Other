4870f540ef2de5854a8ea54ef4b82507693e8fc5
Alexey R <alexey_r.98@ngs.ru>
2022-03-21 16:23:08 +0700

    Fix: Add profile

--- installEnv.sh
+++ installEnv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -vf ~/.bashrc ~/.local/share/mc/bashrc ~/.tmux.conf
 cp -vf bash/.bashrc ~/.bashrc
-cp -vf bash/.profile ~/.profile
+cp -vf bash/.profile ~/.bash_profile
 mkdir -vp ~/.githooks
 cp -vf git/hooks/* ~/.githooks/
 cp -vf tmux/.tmux.conf ~/.tmux.conf
