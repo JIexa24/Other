ecdf655a0b30c0a55ba1f53499b058e49db339fb
Alexey R <alexey_r.98@ngs.ru>
2022-05-15 18:49:10 +0700

    Fix: install env and terraform

--- installEnv.sh
+++ installEnv.sh
@@ -3,12 +3,13 @@ rm -vf ~/.bashrc ~/.local/share/mc/bashrc ~/.tmux.conf
 cp -vf bash/.bashrc ~/.bashrc
 cp -vf bash/.bash_profile ~/.bash_profile
 cp -vf bash/.profile ~/.profile
+cp -vf terraform/.terraformrc ~/.terraformrc
 mkdir -vp ~/.githooks
 cp -vf git/hooks/* ~/.githooks/
 cp -vf tmux/.tmux.conf ~/.tmux.conf
 mkdir -vp ~/.bashrc.d
-[ -d "./bash/.bashrc.d" ] && cp -vf bash/.bashrc.d/*.sh ~/.bashrc.d
+cp -vf bash/.bashrc.d/*.sh ~/.bashrc.d
 ./git/gitalias.sh
 chmod -v 700 ~
 # deprecated
-rm -f ~/.bashrc.d/{ansible,autoenv,aws,go,nvm}.sh
+rm -fv ~/.bashrc.d/{ansible,autoenv,aws,go,nvm}.sh
