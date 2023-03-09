ce8e6cbdb4509071566c58eb34a55f263181d45b
Alexey R <alexey_r.98@ngs.ru>
2023-03-10 02:27:25 +0700

    Fix: bind user

--- passwords-check/freeipa.sh
+++ passwords-check/freeipa.sh
@@ -2,8 +2,9 @@
 # Script for request freeipa password.
 # ./freeipa.sh -h ldap://ipa.example.com -b "cn=users,cn=accounts,dc=example,dc=com" | freeipa-decoder.py
 
-while getopts "b:h:" opt; do
+while getopts "b:h:u:" opt; do
   case $opt in
+  u) BIND_USER=${OPTARG} ;;
   b) BASE_DN=${OPTARG} ;;
   h) HOST=${OPTARG} ;;
   esac
@@ -11,6 +12,7 @@ done
 
 BASE_DN=${BASE_DN:-}
 HOST=${HOST:-}
+BIND_USER=${BIND_USER:-"cn=Directory Manager"}
 
 if [ -z ${BASE_DN} ]; then
   echo "BASE_DN (-b) for users must be specified"
@@ -22,4 +24,4 @@ if [ -z ${HOST} ]; then
   exit 1
 fi
 
-ldapsearch -H "${HOST}" -D "cn=Directory Manager" -b "${BASE_DN}" -W "(!(nsaccountlock=TRUE))" uid userPassword
+ldapsearch -H "${HOST}" -D "${BIND_USER}" -b "${BASE_DN}" -W "(!(nsaccountlock=TRUE))" uid userPassword
